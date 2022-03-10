5cc962098a01379a81df219dc90e2f633705199f
IwonaKrysp <iwona.krysp@gmail.com>
2022-03-10 13:23:50 +0000

    exercise3

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,25 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+let log = console.log;
+let bill = 0;
+
+let calcTotal = (quantity, unitPrice) => {
+  let total = 0;
+  total = quantity * unitPrice;
+  return total;
+};
+
+  log("QTY     ITEM         TOTAL");
+
+  order.forEach(({ itemName, quantity, unitPrice }) => {
+
+    let total = calcTotal(quantity, unitPrice);
+    bill = bill + total;
+ 
+    log(`${quantity}     ${itemName}       ${total}`);
+  });
+ 
+  log(`Bill:`, bill);
+
